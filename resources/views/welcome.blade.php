c323698e6965492fee51dc35c1152c4411d53ef5
cedrix-ravelo <cedrixravelo@gmail.com>
2025-03-13 09:03:37 +0800

    pushing my files

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Project 1</title>
+</head>
+<body>
+<h1>Welcome to my first Project in Laravel11</h1>
+<p>click the button bellow to view the chicken nuggets</p>
+
+<a href="" class="btn">
+    Find Chicken Nuggets!!
+</a>
+</body>
+</html>
